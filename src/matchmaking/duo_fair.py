bc45645d2720b8c2aad02b0cd8d7892439aa855a
Pascal Leroy <pjr.leroy@gmail.com>
2020-04-10 11:06:55 +0200

    save

--- src/matchmaking/duo_fair.py
+++ src/matchmaking/duo_fair.py
@@ -6,5 +6,5 @@ class DuoFair(Matchmaking):
     def list_combat(self, agent_dict, n_matches):
         assert n_matches % 2 == 0
 
-        return [[0, 1] for _ in range(n_matches/2)]\
-               + [[1, 0] for _ in range(n_matches/2)]
+        return [[0, 1] for _ in range(int(n_matches/2))]\
+               + [[1, 0] for _ in range(int(n_matches/2))]
