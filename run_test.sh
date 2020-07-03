3725a701018e458226d099cfec070f41bede3ebe
Pascal Leroy <pascal@vega.ulg.ac.be>
2020-07-03 22:23:44 +0200

    save test scripts

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 source env/bin/activate
-python3 src/main.py --config=qmix --env-config=sc2 with env_args.map_name=3m use_tensorboard=True
+set -x
+file1="/home/pascal/sc2/backup_results_wartank/models/population_alone_qmix_$1/agent_id_0"
+file2="/home/pascal/sc2/backup_results_wartank/models/population_qmix_vs_heuristic_$2/agent_id_0"
+name_var="population_duo_qmix_A$1_vs_H$2"
+python3 src/main_popu_to_test.py \
+--config=popu_duo_qmix_test --env-config=sc2_multi \
+with 'env_args.map_name=3m_multi' \
+'name='$name_var \
+'agent_type_1.checkpoint_path=["'$file1'", "'$file2'"]'
 deactivate
