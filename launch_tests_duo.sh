75565b7e48062eee7f4fab2a33364facc1819626
Pascal Leroy <pascal@vega.ulg.ac.be>
2020-07-07 16:29:55 +0200

    save tests

--- launch_tests_duo.sh
+++ launch_tests_duo.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-for (( i=1; i<=10; i++ ))
+for (( i=3; i<8; i++ ))
 do
-  for (( j=1; j<= 10; j++))
+  for (( j=3; j<= 8; j++))
   do
-  ./run_test.sh $i $j
+  ./run_test_duo.sh $i $j
   done
 done
--- run_test_duo.sh
+++ run_test_duo.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 source env/bin/activate
 set -x
-file1="/home/pascal/sc2/backup_results_wartank/models/population_alone_qmix_$1/agent_id_0"
-file2="/home/pascal/sc2/backup_results_wartank/models/population_qmix_vs_heuristic_$2/agent_id_0"
-name_var="population_duo_qmix_A$1_vs_H$2"
+file1="/home/pascal/sc2/backup_results_wartank/models/population_alone_coma_$1/agent_id_0"
+file2="/home/pascal/sc2/backup_results_wartank/models/population_coma_vs_heuristic_$2/agent_id_0"
+name_var="population_duo_coma_A$1_vs_H$2"
 python3 src/main_popu_to_test.py \
---config=popu_duo_qmix_test --env-config=sc2_multi \
+--config=popu_duo_coma_test --env-config=sc2_multi \
 with 'env_args.map_name=3m_multi' \
 'name='$name_var \
 'agent_type_1.checkpoint_path=["'$file1'", "'$file2'"]'
