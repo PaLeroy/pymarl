fa800d45173892170aa7b6e2acd5b50938bc07c3
Pascal <pjr.leroy@gmail.com>
2020-08-31 20:06:37 +0200

    save test config

--- run_test_duo.sh
+++ run_test_duo.sh
@@ -7,6 +7,9 @@ name_var="population_test_duo_$3_A$1_vs_H$2"
 timeout 15m python3 src/main_popu_to_test.py \
 --config=popu_duo_$3_test --env-config=sc2_multi \
 with 'env_args.map_name=3m_multi' \
+'env_args.log_more_stats=True' \
 'name='$name_var \
 'agent_type_1.checkpoint_path=["'$file1'", "'$file2'"]'
 deactivate
+
+sleep 10
